49d00f2aaa2c0f52b625c0cf1af322cc44469f01
wooyoojoo <1667279083@qq.com>
2023-07-15 21:42:19 +0800

    为啥科技好人工服务了客人会替你哦怕、

--- GBitGameJam/Assets/Script/RotateScript.cs
+++ GBitGameJam/Assets/Script/RotateScript.cs
@@ -7,21 +7,42 @@ using UnityEngine.UIElements;
 
 namespace Script
 {
+    public enum RotateAxis
+    {
+        X,
+        Y,
+        Z
+    }
     public abstract class RotateScript : MonoBehaviour
     {
         [Header("父物体旋转一圈时间")] public float rotateSpeed = 5; //本体旋转速度
         [Header("是否连续旋转")] public bool continueRotate = false;
+        public RotateAxis Axis;
         public Transform pointer;
         protected float counting = 1f; //倒计时时间
         protected TweenerCore<Quaternion, Vector3, QuaternionOptions> _tweenerCore;
         protected bool ableToRotate = true;
-
-
+        protected Vector3 rotateAxis;
+        private void Start()
+        {
+            switch (Axis)
+            {
+                case RotateAxis.X:
+                    rotateAxis = new Vector3(1, 0, 0);
+                    break;
+                case RotateAxis.Y:
+                    rotateAxis = new Vector3(0, 1, 0);
+                    break;
+                case RotateAxis.Z:
+                    rotateAxis = new Vector3(0, 0, 1);
+                    break;
+            }
+        }
         protected virtual void RotateFunction()
         {
             if (continueRotate && ableToRotate)
             {
-                transform.Rotate(new Vector3(1, 0, 0), rotateSpeed * Time.deltaTime);
+                transform.Rotate(rotateAxis, rotateSpeed * Time.deltaTime);
             }
         }
 
@@ -35,7 +56,7 @@ namespace Script
         {
             DOTween.Kill(_tweenerCore);
 
-            _tweenerCore = target.DOLocalRotate(new Vector3(-angle, 0, 0), time, RotateMode.WorldAxisAdd);
+            _tweenerCore = target.DOLocalRotate(-rotateAxis * angle, time, RotateMode.WorldAxisAdd);
 
             if (callEvent)
                 _tweenerCore.OnComplete(EventHandler.CallAfterJumpFinish);
---
+++ GBitGameJam/Assets/Script/Telephone.cs
@@ -0,0 +1,113 @@
+using Script;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Telephone : BaseClock
+    {
+        private GameObject _platform;
+        [SerializeField] private Vector2 judgmentArea = new Vector2(30, 90);
+        [SerializeField] private float rotateAngle;
+        [SerializeField] private Vector2 judgmentLength = new Vector2(5, 20);
+        private float _jumpAngle;
+        private float _randomAngle;
+        private static readonly int Preparing = Animator.StringToHash("Preparing");
+        private static readonly int Shake = Animator.StringToHash("Shake");
+        private Animator _animator;
+
+        [SerializeField] private float _forceBeforeJump = 0;
+        private float _holdTimeBeforeJump = 0;
+
+        protected override void ChildStart()
+        {
+            GenerateStartJumpPoint();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void OnEnable()
+        {
+            EventHandler.AfterJumpFinish += JumpAreaChecker;
+            EventHandler.BeforeJumpStart += BeforeJumpStart;
+            EventHandler.AfterJumpFail += AfterJumpFail;
+        }
+
+        private void OnDisable()
+        {
+            EventHandler.AfterJumpFinish -= JumpAreaChecker;
+            EventHandler.BeforeJumpStart -= BeforeJumpStart;
+            EventHandler.AfterJumpFail -= AfterJumpFail;
+        }
+
+        private void BeforeJumpStart()
+        {
+            _forceBeforeJump = angle;
+            _holdTimeBeforeJump = holdingTime;
+        }
+
+        private void AfterJumpFail()
+        {
+            rotateScript.RotateAngle(_forceBeforeJump, Pointer, _holdTimeBeforeJump / 2);
+        }
+
+        protected override void GenerateStartJumpPoint()
+        {
+            rotateAngle = Random.Range(judgmentArea.x, judgmentArea.y);
+            _randomAngle = Random.Range(judgmentLength.x, judgmentLength.y);
+
+            _platform = Instantiate((GameObject)Resources.Load("Prefabs/Platform/Pointer"), transform);
+
+            _platform.GetComponent<SkinnedMeshRenderer>().enabled = false;
+
+            _platform.transform.rotation = Pointer.rotation;
+
+            _platform.transform.Rotate(new Vector3(rotateAngle, 0, 0));
+
+            Vector3 scale = _platform.transform.GetChild(0).localScale;
+            _platform.transform.GetChild(0).localScale = new Vector3(scale.x, scale.y * _randomAngle / 2.7f, scale.z);
+        }
+
+        protected override void JumpAreaChecker()
+        {
+            float targetAngle = Quaternion.Angle(_platform.transform.localRotation, Pointer.localRotation);
+
+            if (targetAngle <= _randomAngle)
+            {
+                Destroy(_platform);
+                GenerateStartJumpPoint();
+            }
+            else
+            {
+                EventHandler.CallAfterJumpFail();
+            }
+        }
+
+        protected override void BeforeJump()
+        {
+            //_animator.SetBool(Preparing, true);
+        }
+
+        protected override void Jump()
+        {
+            EventHandler.CallBeforeJumpStart(); //ÌøÔ¾¿ªÊ¼
+
+            rotateScript.RotateAngle(-angle, Pointer, holdingTime / 5, true);
+            //_animator.SetTrigger(Shake);
+            //_animator.SetBool(Preparing, false);
+
+            angle = 0;
+            holdingTime = 0;
+        }
+
+        protected override void PrepareJump()
+        {
+            angle += force * Time.unscaledDeltaTime;
+            holdingTime += Time.unscaledDeltaTime;
+        }
+
+    }
+}
